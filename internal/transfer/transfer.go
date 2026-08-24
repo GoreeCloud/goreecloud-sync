0192811f4af989b3fc71dfac11e2fe48d7de96e9
GoreeCloud <295260680+GoreeCloud@users.noreply.github.com>
2026-08-24 11:15:32 -0500

    Remove duplicate transfer chunk-size constant

--- internal/transfer/transfer.go
+++ internal/transfer/transfer.go
@@ -6,9 +6,6 @@ import (
 	"io"
 )
 
-// ChunkSize defines the default transfer block size.
-const ChunkSize = 1024 * 1024
-
 // HashReader calculates a SHA-256 digest for transferred data.
 func HashReader(r io.Reader) (string, error) {
 	h := sha256.New()
